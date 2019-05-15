5cae4bb1cad2d62dac53384a78a9ce0c4ff19d18
ZoneTwelve <zonetwelve159@gmail.com>
2019-05-15 18:02:43 +0800

    i dont know

--- src/game.java
+++ src/game.java
@@ -1,12 +1,36 @@
 import processing.core.PApplet;
+import processing.core.PVector;
+import processing.core.PImage;
+import java.util.ArrayList;
 public class game extends PApplet{
+  actorObject player;
+  actorObject emenys[];
   public static void main(String[] args) {
       PApplet.main("game");
   }
+  public void settings(){
+    fullScreen();
+    //size(600, 600);
+  }
 
-  public void settings(){size(600, 600);}
-
-  public void setup(){}
+  public void setup(){
+    
+  }
 
   public void draw(){}
+  
+  public void gameing(){
+    
+  }
+
+  class actorObject{
+    PImage style;
+    PVector pos;
+    actorObject(){
+      
+    }
+    void display(){
+      image(style, pos.y, pos.x);
+    }
+  }
 }
