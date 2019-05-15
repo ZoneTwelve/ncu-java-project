9ae135053669cfcc2d2a133dd574c862346a1f17
ZoneTwelve <zonetwelve159@gmail.com>
2019-05-15 17:27:19 +0800

    setup

---
+++ src/finalproject/game.java
@@ -0,0 +1,14 @@
+package finalproject;
+import processing.core.PApplet;
+
+public class game extends PApplet{
+  public static void main(String[] args) {
+      PApplet.main("ZoneGame");
+  }
+
+  public void settings(){size(600, 600);}
+
+  public void setup(){}
+
+  public void draw(){}
+}
